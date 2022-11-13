db30523d00cc59b89defbf9f5d1a7267f0a386e4
filo12345678910 <filip.firkowski@gmail.com>
2022-11-13 23:30:48 +0100

    Added Zoo class

--- zoo.cpp
+++ zoo.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
+#include <typeinfo>
+#include <vector>
 #include "zoo.h"
 using namespace zoo;
 using namespace std;
 Animal::Animal(int age, string gender){this->age = age;this->gender = gender;}
 Amphibiant::Amphibiant(int age, string gender, string color_of_skin): Animal(age, gender){this->color_of_skin = color_of_skin;}
 Salamander::Salamander(int age, string gender, string color_of_skin, int tail_length, string color_of_dots): Amphibiant(age, gender, color_of_skin){this->tail_length = tail_length; this->color_of_dots = color_of_dots;}
+Frog::Frog(int age, string gender, string color_of_skin, bool poisonous): Amphibiant(age, gender, color_of_skin){this->poisonous = poisonous;}
+
 void Animal::toString()
 {
     cout << "age: " << age << endl;
@@ -22,10 +26,22 @@ void Salamander::toString()
     cout << "tail_length: " << tail_length << endl;
     cout << "color_of_dots: " << color_of_dots << endl;
 }
+void Frog::toString()
+{
+    cout << "Frog statistics:" << endl;
+    Amphibiant::toString();
+    cout << "is_poisonous?: " << poisonous << endl;
+}
 int main()
 {
     Salamander* cat1 = new Salamander(13, "male", "black", 23, "red");
     Salamander* cat2 = new Salamander(34, "female", "black", 3, "blue");
-    cat2->toString();
-    cat1->toString();
+    Frog* cat3 = new Frog(23, "male", "red", 1);
+
+
+    Zoo<Animal*> p;
+    p.Add(cat3);
+    p.Add(cat2);
+    p.Show();
+
 }
--- zoo.h
+++ zoo.h
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <vector>
 namespace zoo
 {
     class Animal
@@ -9,19 +10,48 @@ namespace zoo
         Animal(int age, std::string gender);
         void virtual toString();
     };
-    class Amphibiant: Animal
+    class Amphibiant: public Animal
     {
         std::string color_of_skin;
     public:
         Amphibiant(int age, std::string gender,std::string color_of_skin);
         void virtual toString();
     };
-    class Salamander: Amphibiant
+    class Salamander: public Amphibiant
     {
         int tail_length;
         std::string color_of_dots;
     public:
         Salamander(int age, std::string gender,std::string color_of_skin,int tail_length,std::string color_of_dots);
         void toString();
+    };
+    class Frog: public Amphibiant
+    {
+        bool poisonous;
+    public:
+        Frog(int age, std::string gender,std::string color_of_skin,bool poisonous);
+        void toString();
+    };
+
+    template <class T>
+    class Zoo
+    {
+    public:
+        std::vector<Animal*> Animals;
+        void Add(T value);
+        void Show();
+    };
+    template <class T>
+    void Zoo<T>::Add(T value)
+    {
+        this->Animals.push_back(value);
+    };
+    template <class T>
+    void Zoo<T>::Show()
+    {
+        for(int i = 0; i < this->Animals.size(); i++)
+        {
+            this->Animals[i]->toString();
+        }
     };
 }
